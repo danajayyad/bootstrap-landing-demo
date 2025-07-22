a00844c2490afd535b39a8dc79959cfc66bae316
danajayyad <danayyad2111@gmail.com>
2025-07-22 20:25:14 +0400

    cards

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-document.getElementById('closeNavbar').addEventListener('click', function () {
-  document.querySelector('.navbar-toggler').click();
-});
+const closeBtn = document.getElementById('closeNavbar');
+const toggler = document.querySelector('.navbar-toggler');
+const navbar = document.getElementById('navbar');
+const overlay = document.getElementById('overlay');
+
+
+  navbar.addEventListener('shown.bs.collapse', () => {
+    overlay.style.display = 'block';
+  });
+
+  navbar.addEventListener('hidden.bs.collapse', () => {
+    overlay.style.display = 'none';
+  });
+
+  closeBtn.addEventListener('click', () => {
+    toggler.click();
+  });
